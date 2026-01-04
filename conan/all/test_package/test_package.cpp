09ac7a689e1f73caf4aa390b5fecdc26f01fb049
Jimbopython <le.stoffers@gmail.com>
2026-01-04 13:57:27 +0100

    update conan package

--- conan/all/test_package/test_package.cpp
+++ conan/all/test_package/test_package.cpp
@@ -1,8 +1,17 @@
-#include <sfdm/decode_result.hpp>
+#include <sfdm/sfdm.hpp>
 
 
 int main(void) {
     sfdm::DecodeResult{};
+#ifdef SFDM_WITH_ZXING_DECODER
+    sfdm::ZXingCodeReader{};
+#endif
+#ifdef SFDM_WITH_LIBDMTX_DECODER
+    sfdm::LibdmtxCodeReader{};
+#endif
+#if defined(SFDM_WITH_ZXING_DECODER) && defined(SFDM_WITH_LIBDMTX_DECODER)
+    sfdm::LibdmtxZXingCombinedCodeReader{};
+#endif
 
     return 0;
 }
